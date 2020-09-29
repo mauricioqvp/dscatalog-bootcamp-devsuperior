88a28a61016f30d6af287cb5de32e1f588e737db
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-09-29 20:41:49 -0300

    POST new Category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -22,18 +22,20 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		List<Category> list = repository.findAll();
-		/*
-		 * List<CategoryDTO> listDto = new ArrayList<>(); for(Category cat : list) {
-		 * listDto.add(new CategoryDTO(cat)); }
-		 * 
-		 * return listDto;
-		 */
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
 		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
+	}
+	
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
 		return new CategoryDTO(entity);
 	}
 }
