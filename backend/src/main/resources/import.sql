715edccb5303f0ac373607ad143a961162303e7a
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-09-30 01:15:16 -0300

    Auditing data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_category (NAME) VALUES ('Books');
-INSERT INTO tb_category (NAME) VALUES ('Eletronics');
-INSERT INTO tb_category (NAME) VALUES ('Computers');
+INSERT INTO tb_category (NAME, created_At) VALUES ('Books', NOW());
+INSERT INTO tb_category (NAME, created_At) VALUES ('Eletronics', NOW());
+INSERT INTO tb_category (NAME, created_At) VALUES ('Computers', NOW());
