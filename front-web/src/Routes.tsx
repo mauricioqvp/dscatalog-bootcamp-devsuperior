20ff22a43a4fe05dd5f013a5e8220edad03a417e
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-10-08 01:34:29 -0300

    Finishing the product details page

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Catalog from './pages/Catalog';
 import Admin from './pages/Admin';
 import Navbar from './core/components/Navbar';
+import ProductDetails from './pages/Catalog/components/ProductDetails';
 
 const Routes = () => (
     <BrowserRouter>
@@ -12,8 +13,11 @@ const Routes = () => (
             <Route path="/" exact>
                 <Home />
             </Route>
-            <Route path="/catalog">
+            <Route path="/products" exact>
                 <Catalog />
+            </Route>
+            <Route path="/products/:productId">
+                <ProductDetails />
             </Route>
             <Route path="/admin">
                 <Admin />
