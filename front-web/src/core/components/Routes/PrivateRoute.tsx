8554abe1c5fd3eecfe59e40fd676f5d6b489ea58
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-11-24 10:18:46 -0300

    Add allowed roles on private route

---
+++ front-web/src/core/components/Routes/PrivateRoute.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { isAllowebByRole, isAuthenticated, Role } from 'core/utils/auth';
+import { Redirect, Route } from 'react-router-dom';
+
+type Props = {
+    children: React.ReactNode;
+    path: string;
+    allowedRoutes?: Role[];
+}
+
+const PrivateRoute = ({ children, path, allowedRoutes }: Props) => {
+    return (
+      <Route
+        path={path}
+        render={({ location }) => {
+            if (!isAuthenticated()) {
+              return (
+                <Redirect
+                  to={{
+                    pathname: "/admin/auth/login",
+                    state: { from: location }
+                  }}
+                />
+              )
+            } else if (isAuthenticated() && !isAllowebByRole(allowedRoutes)) {
+              return (
+                <Redirect to={{ pathname: "/admin" }} />
+              )
+            }
+            return children;
+        }}
+      />
+    );
+  }
+
+  export default PrivateRoute;
--- front-web/src/core/utils/auth.ts
+++ front-web/src/core/utils/auth.ts
@@ -1,3 +1,4 @@
+import jwtDecode from 'jwt-decode';
 export const CLIENT_ID = 'dscatalog';
 export const CLIENT_SECRET = 'dscatalog123';
 
@@ -10,6 +11,14 @@ type LoginResponse = {
     userId: number;
 }
 
+export type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
+
+type AccessToken = {
+    exp: number;
+    user_name: string;
+    authorities: Role[];
+}
+
 export const saveSessionData = (loginResponse: LoginResponse) => {
     localStorage.setItem('authData', JSON.stringify(loginResponse));
 }
@@ -19,4 +28,35 @@ export const getSessionData = () => {
     const parsedSessionData = JSON.parse(sessionData);
 
     return parsedSessionData as LoginResponse;
+}
+
+export const getAccessTokenDecoded = () => {
+    const sessionData = getSessionData();
+
+    const tokenDecoded = jwtDecode(sessionData.access_token);
+    return tokenDecoded as AccessToken;
+}
+
+export const isTokenValid = () => {
+    const { exp } = getAccessTokenDecoded();
+
+    return Date.now() <= exp * 1000;
+}
+
+export const isAuthenticated = () => {
+    const sessionData = getSessionData();
+
+    return sessionData.access_token && isTokenValid();
+    // "autData" no localStorage
+    // accesss_token não pode estar expirado
+}
+
+export const isAllowebByRole = (routeRoles: Role[] = []) => {
+    if (routeRoles.length === 0) {
+        return true;
+    }
+
+    const { authorities } = getAccessTokenDecoded();
+    
+    return routeRoles.some(role => authorities.includes(role));
 }
