e7f050fc178b08ce6ecf976dec5d11efd8dab565
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-11-24 21:03:33 -0300

    Cap 5-21

--- front-web/src/core/components/Navbar/index.tsx
+++ front-web/src/core/components/Navbar/index.tsx
@@ -4,12 +4,12 @@ import './styles.scss';
 
 const Navbar = () => (
     <nav className="row bg-primary main-nav">
-        <div className="col-2">
+        <div className="col-3">
             <Link to="/" className="nav-logo-text">
                 <h4>DS Catalog</h4>
             </Link>
         </div>
-        <div className="col-6 offset-2">
+        <div className="col-6">
             <ul className="main-menu">
                 <li>
                     <NavLink to="/" exact>HOME</NavLink>
@@ -21,6 +21,9 @@ const Navbar = () => (
                     <NavLink to="/admin">ADMIN</NavLink>
                 </li>
             </ul>
+        </div>
+        <div className="col-3 text-right">
+            login
         </div>
     </nav>
 );
