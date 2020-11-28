f6d4cbd3d7affe9173caba7d596d0d0b056acb9a
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-11-28 03:47:02 -0300

    Cap 5-27

---
+++ front-web/src/pages/Admin/components/Products/Card/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './styles.scss';
+
+const Card = () => {
+    return (
+        <div className="card-base product-card-admin">
+            <div className="row">
+                <div className="col-2 text-center border-right py-3">
+                <img 
+                    src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/3-big.jpg"
+                    alt="Produto teste"
+                    className="product-card-image-admin"
+                />
+                </div>
+                <div className="col-7 py-3">
+                    <h3 className="product-card-name-admin">
+                        Computador i7
+                    </h3>
+                </div>
+                <div className="col-3 py-3">
+                    <h1>Ações</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
