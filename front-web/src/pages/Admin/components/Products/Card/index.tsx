79da7aae1ececd6a029e903a1c734511fdffcde7
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-11-28 21:02:35 -0300

    Cap 5-32

--- front-web/src/pages/Admin/components/Products/Card/index.tsx
+++ front-web/src/pages/Admin/components/Products/Card/index.tsx
@@ -1,6 +1,7 @@
 import ProductPrice from 'core/components/ProductPrice';
 import { Product } from 'core/types/Products';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 type Props = {
@@ -36,12 +37,12 @@ const Card = ({ product }: Props) => {
                     </div>
                 </div>
                 <div className="col-3 pt-3 pr-5">
-                    <button 
+                    <Link to={`/admin/products/${product.id}`}
                         type="button" 
                         className="btn btn-outline-secondary btn-block border-radius-10 mb-3 btn-edit"
                     >
                         EDITAR
-                    </button>
+                    </Link>
                     <button 
                         type="button" 
                         className="btn btn-outline-danger btn-block border-radius-10"
